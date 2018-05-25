19b6156f7608113ebf04b6948359d611eda591eb
Ambrosini Loïc <crazyball@users.noreply.github.com>
2018-05-25 10:24:28 +0200

    Merge pull request #385 from aya/infra/add-pdns-vsftpd
    story #88 : Infra - add pdns and vsftpd docker images

--- stack/dev/docker/pdns-server/docker-entrypoint.sh
+++ stack/dev/docker/pdns-server/docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 trap 'kill -SIGQUIT $PID' INT
 
-# Launch pdns_server
+# Launch pdns_recursor by default
 [ $# -eq 0 ] && /usr/local/sbin/pdns_recursor || exec "$@" &
 PID=$! && wait
