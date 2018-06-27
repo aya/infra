7a1773aa7fefcc667beb5d262200e0ccdea20834
Yann Autissier <yann.autissier@gmail.com>
2018-06-27 17:59:02 +0200

    Merge tag 'v3.54.1' into develop

--- stack/dev/docker/alertmanager/docker-entrypoint.sh
+++ stack/dev/docker/alertmanager/docker-entrypoint.sh
@@ -5,9 +5,6 @@ set -x
 
 trap 'kill -SIGQUIT $PID' INT
 
-sed 's@SLACK_WEBHOOK_ID@'"${SLACK_WEBHOOK_ID:-UNDEFINED}"'@g' /etc/alertmanager/config.tmpl > /etc/alertmanager/alertmanager.yml
-
-
 # Launch alertmanager by default, or paramater
 [ $# -eq 0 ] && /bin/alertmanager --storage.path=/etc/alertmanager || exec "$@" &
 PID=$! && wait
