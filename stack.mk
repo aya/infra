e099fe434f155290980accfc2f7b0a8c40b40316
Yann Autissier <yann.autissier@gmail.com>
2018-08-20 15:06:08 +0200

    #86 bind mysql to port 3306 for local ENV

--- stack.mk
+++ stack.mk
@@ -26,6 +26,7 @@ endif
 
 stack-%: ## Start docker stack
 	$(eval COMPOSE_FILE:=$(COMPOSE_FILE) stack/$*/docker-compose.yml)
+	$(if $(wildcard stack/$*/docker-compose.$(ENV).yml), $(eval COMPOSE_FILE:=$(COMPOSE_FILE) stack/$*/docker-compose.$(ENV).yml))
 	$(if $(wildcard stack/$*/.env.dist), $(call .env,stack/$*) $(eval ENV_FILE:=$(ENV_FILE) stack/$*/.env))
 
 start-up: ssh-add
