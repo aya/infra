c18e8e6f755568de23df0b7db8ff0df2b5667f2c
Yann Autissier <yann.autissier@gmail.com>
2019-01-08 17:36:02 +0100

    #462 setup NFS on OSX
    * configure and enable nfsd on macos
    * export monorepo

--- stack/services.mk
+++
@@ -1,4 +0,0 @@
-services-sysctl:
-	$(call docker-run,--privileged alpine:latest,sysctl -w vm.max_map_count=$(SYSCTL_VM_MAX_MAP_COUNT) >/dev/null) # elasticsearch
-	$(call docker-run,--privileged alpine:latest,sysctl -w vm.overcommit_memory=1 >/dev/null) # redis
-	$(call docker-run,--privileged alpine:latest,/bin/sh -c 'echo never > /sys/kernel/mm/transparent_hugepage/enabled') # redis
