a2847fba5e444c7c3348b0a5fb2992ba0f8acab3
Yann Autissier <yann.autissier@gmail.com>
2019-01-08 17:36:02 +0100

    #462 setup NFS on OSX
    * configure and enable nfsd on macos
    * export monorepo

--- def.mk
+++ def.mk
@@ -1,5 +1,4 @@
 APP                             ?= $(SUBREPO)
-APP_DIR                         ?= $(CURDIR)
 BRANCH                          ?= $(shell git rev-parse --abbrev-ref HEAD)
 COMMIT                          ?= $(shell git rev-parse HEAD)
 CONTEXT                         ?= $(shell awk 'BEGIN {FS="="}; $$1 !~ /^(\#|$$)/ {print $$1}' .env.dist 2>/dev/null) BRANCH COMMIT TAG UID USER
@@ -9,11 +8,12 @@ DRONE                           ?= false
 ENV                             ?= local
 ENV_FILE                        ?= .env
 ENV_RESET                       ?= false
-ENV_SYSTEM                       = $(shell printenv |awk -F '=' 'NR == FNR { if($$1 !~ /^(\#|$$)/) { A[$$1]; next } } ($$1 in A)' .env.dist - 2>/dev/null |awk '{print} END {print "APP=$(APP)\nBRANCH=$(BRANCH)\nCOMMIT=$(COMMIT)\nCOMPOSE_IGNORE_ORPHANS=$(COMPOSE_IGNORE_ORPHANS)\nCOMPOSE_PROJECT_NAME=$(COMPOSE_PROJECT_NAME)\nCOMPOSE_SERVICE_NAME=$(COMPOSE_SERVICE_NAME)\nDOCKER_IMAGE_REPO=$(DOCKER_IMAGE_REPO)\nDOCKER_IMAGE_TAG=$(DOCKER_IMAGE_TAG)\nENV=$(ENV)\nGID=$(GID)\nMONOREPO_DIR=$(MONOREPO_DIR)\nUID=$(UID)\nUSER=$(USER)\nTAG=$(TAG)"}' |awk -F "=" '!seen[$$1]++')
+ENV_SYSTEM                       = $(shell printenv |awk -F '=' 'NR == FNR { if($$1 !~ /^(\#|$$)/) { A[$$1]; next } } ($$1 in A)' .env.dist - 2>/dev/null |awk '{print} END {print "APP=$(APP)\nBRANCH=$(BRANCH)\nCOMMIT=$(COMMIT)\nCOMPOSE_IGNORE_ORPHANS=$(COMPOSE_IGNORE_ORPHANS)\nCOMPOSE_PROJECT_NAME=$(COMPOSE_PROJECT_NAME)\nCOMPOSE_SERVICE_NAME=$(COMPOSE_SERVICE_NAME)\nDOCKER_IMAGE_REPO=$(DOCKER_IMAGE_REPO)\nDOCKER_IMAGE_TAG=$(DOCKER_IMAGE_TAG)\nENV=$(ENV)\nGID=$(GID)\nMONOREPO_DIR=$(MONOREPO_DIR)\nSUBREPO_DIR=$(SUBREPO_DIR)\nTAG=$(TAG)\nUID=$(UID)\nUSER=$(USER)"}' |awk -F "=" '!seen[$$1]++')
 GID                             ?= $(shell id -g)
 MONOREPO                        ?= $(if $(wildcard .git),$(notdir $(CURDIR)),$(notdir $(realpath $(CURDIR)/..)))
 MONOREPO_DIR                    ?= $(if $(wildcard .git),$(CURDIR),$(realpath $(CURDIR)/..))
 SUBREPO                         ?= $(notdir $(CURDIR))
+SUBREPO_DIR                     ?= $(CURDIR)
 TAG                             ?= $(shell git tag -l --points-at HEAD)
 UID                             ?= $(shell id -u)
 USER                            ?= $(shell id -nu)
@@ -26,4 +26,19 @@ ifneq ($(filter $(CMDS),$(firstword $(MAKECMDGOALS))),)
 ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
 # ...and turn them into do-nothing targets
 $(eval $(ARGS):;@:)
+endif
+
+# Guess OS
+ifeq ($(OSTYPE),cygwin)
+HOST_SYSTEM                     := CYGWIN
+else ifeq ($(OS),Windows_NT)
+HOST_SYSTEM                     := WINDOWS
+else
+UNAME_S := $(shell uname -s 2>/dev/null)
+ifeq ($(UNAME_S),Linux)
+HOST_SYSTEM                     := LINUX
+endif
+ifeq ($(UNAME_S),Darwin)
+HOST_SYSTEM                     := DARWIN
+endif
 endif
--- stack.mk
+++ stack.mk
@@ -2,7 +2,14 @@
 # STACK
 include stack/*.mk
 
-bootstrap-docker: docker-network services-sysctl
+bootstrap-docker: docker-network update-sysctl
+ifeq ($(HOST_SYSTEM),DARWIN)
+	$(call setup-osx-nfs)
+endif
+
+update-sysctl:
+	$(call docker-run,--privileged alpine:latest,/bin/sh -c 'echo never > /sys/kernel/mm/transparent_hugepage/enabled')
+	$(foreach config,$(SYSCTL),$(call docker-run,--privileged alpine:latest,sysctl -w $(config)) &&) true
 
 stack: $(patsubst %,stack-%,$(STACK)) bootstrap
 	$(call .env)
@@ -13,3 +20,13 @@ stack-%: ## Start docker stack
 	$(if $(wildcard stack/$*/.env.dist), $(call .env,stack/$*) $(eval ENV_FILE:=$(ENV_FILE) stack/$*/.env))
 
 start-up: base-ssh-add
+
+define setup-osx-nfs
+	$(eval dir:=$(or $(1),$(MONOREPO_DIR)))
+	$(eval uid:=$(or $(2),$(UID)))
+	$(eval gid:=$(or $(3),$(GID)))
+	grep "$(dir)" /etc/exports >/dev/null 2>&1 || echo "$(dir) -alldirs -mapall=$(uid):$(gid) localhost" |sudo tee -a /etc/exports >/dev/null
+	$(foreach config,$(OSX_NFS),grep "$(config)" /etc/nfs.conf >/dev/null 2>&1 || echo "$(config)" |sudo tee -a /etc/nfs.conf >/dev/null &&) true
+	nfsd status >/dev/null || sudo nfsd enable
+	showmount -e localhost |grep "$(dir)" >/dev/null 2>&1 || sudo nfsd restart
+endef
