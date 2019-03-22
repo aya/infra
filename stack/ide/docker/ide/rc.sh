b097d20d377ae80e863a40e73448d5bce25d13be
Yann Autissier <yann.autissier@gmail.com>
2019-03-22 15:39:42 +0100

    custom shell and git config

--- stack/ide/docker/ide/rc.sh
+++ stack/ide/docker/ide/rc.sh
@@ -5,7 +5,7 @@ type force >/dev/null 2>&1 || source /etc/profile.d/rc_functions.sh 2>/dev/null
 case $- in
   # if we are in an interactive shell
   *i*)
-    # load user defined stuffs
+    # load user defined stuffs from ~/.rc.d
     for user_func in ${HOME}/.rc.d/*; do
         # read files only
         [ -f ${user_func} ] && func_name="$(basename ${user_func})" || continue
@@ -17,6 +17,19 @@ case $- in
         done
         # call user function with args passed from the content of the user_func file
         [ -n "${func_name}" ] && ${func_name} $(cat ${user_func}) 2>/dev/null
+    done
+    # load user defined stuffs from RC_* env vars
+    declare 2>/dev/null |awk -F'=' '$1 ~ /^RC_/ {print tolower(substr($1,4)),$2}' |while read func_name func_args; do
+        [ "${func_args}" = "false" ] && continue
+        [ "${func_args}" = "true" ] && unset func_args
+        # at this stage, func_name can start with numbers to allow ordering function calls with file names starting with numbers
+        # func_name must start with a letter, remove all other characters at the beginning of func_name until a letter is found
+        while [ "${func_name}" != "" -a "${func_name#[a-z]}" = "${func_name}" ]; do
+            # remove first char of func_name
+            func_name="${func_name#?}"
+        done
+        # call user function with args passed from the value of the env var
+        [ -n "${func_name}" ] && ${func_name} ${func_args} 2>/dev/null
     done
   ;;
 esac
