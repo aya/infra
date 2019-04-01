702dacc03bca4d7bf2a02c7e3de3f5a03a86e9c6
Yann Autissier <yann.autissier@gmail.com>
2019-04-01 15:51:29 +0200

    move dockerfile and makefile at infra top level

--- def.infra.mk
+++ def.infra.mk
@@ -9,6 +9,7 @@ SETUP_NFSD                      ?= false
 SETUP_NFSD_OSX_CONFIG           ?= nfs.server.bonjour=0 nfs.server.mount.regular_files=1 nfs.server.mount.require_resv_port=0 nfs.server.nfsd_threads=16 nfs.server.async=1
 SETUP_SYSCTL                    ?= true
 SETUP_SYSCTL_CONFIG             ?= vm.max_map_count=262144 vm.overcommit_memory=1 fs.file-max=8388608 net.core.somaxconn=1024
+SHELL                           ?= /bin/sh
 STACK                           ?= services
 STACK_BASE                      ?= base
 STACK_NODE                      ?= node
