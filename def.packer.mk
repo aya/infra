7f85635526e46805929ba8bc8d114776a2b02745
Yann Autissier <yann.autissier@gmail.com>
2019-04-08 16:03:47 +0200

    add zram and aya ssh key to default install

--- def.packer.mk
+++ def.packer.mk
@@ -1,8 +1,8 @@
 ENV_SYSTEM                      += PACKER_CACHE_DIR=cache PACKER_KEY_INTERVAL=10ms PACKER_LOG=1
 KVM_GID                         ?= $(shell getent group kvm |awk -F: '{print $$3}')
-PACKER_BUILD_ARGS               ?= -on-error=cleanup -var template=$(PACKER_TEMPLATE)
-PACKER_VNC_PORT                 ?= 5900
-PACKER_VNC_ADDRESS              ?= 0.0.0.0
+PACKER_BUILD_ARGS               ?= -on-error=cleanup -var template=$(PACKER_TEMPLATE) $(foreach var,arch disk_size id_rsa.pub hostname password release template version,$(if $($(var)),-var $(var)='$($(var))'))
+PACKER_VNC_PORT                 ?= $(if $(vnc_port_max),$(vnc_port_max),5900)
+PACKER_VNC_ADDRESS              ?= $(if $(vnc_bind_address),$(vnc_bind_address),0.0.0.0)
 ifeq ($(DEBUG), true)
 PACKER_BUILD_ARGS               += -debug
 endif
@@ -21,7 +21,7 @@ define packer
 	$(call run,$(DOCKER_SSH_AUTH) $(if $(KVM_GID),--group-add $(KVM_GID)) --device /dev/kvm -v $(HOME):/home/$(USER) -p $(PACKER_VNC_PORT):$(PACKER_VNC_PORT) $(DOCKER_REPO)/packer:$(DOCKER_BUILD_TARGET) $(1))
 endef
 define packer-qemu
-	$(call run,$(if $(KVM_GID),--group-add $(KVM_GID)) --device /dev/kvm -p $(PACKER_VNC_PORT):$(PACKER_VNC_PORT) --entrypoint=qemu-system-x86_64 $(DOCKER_REPO)/packer:$(DOCKER_BUILD_TARGET) -enable-kvm -drive file=$(1)$(comma)format=raw -vnc $(PACKER_VNC_ADDRESS):$(subst 590,,$(PACKER_VNC_PORT)))
+	$(call run,$(if $(KVM_GID),--group-add $(KVM_GID)) --device /dev/kvm -p $(PACKER_VNC_PORT):$(PACKER_VNC_PORT) --entrypoint=qemu-system-x86_64 $(DOCKER_REPO)/packer:$(DOCKER_BUILD_TARGET) -enable-kvm -m 512m -drive file=$(1)$(comma)format=raw -vnc $(PACKER_VNC_ADDRESS):$(subst 590,,$(PACKER_VNC_PORT)))
 endef
 
 else
@@ -30,7 +30,7 @@ define packer
 	$(call run,packer $(1))
 endef
 define packer-qemu
-	$(call run,qemu-system-x86_64 -enable-kvm -drive file=$(1)$(comma)format=raw)
+	$(call run,qemu-system-x86_64 -enable-kvm -m 512m -drive file=$(1)$(comma)format=raw)
 endef
 
 endif
