6a9070e69856ce8620bfe26410617f76a971fb76
Yann Autissier <yann.autissier@gmail.com>
2019-04-09 18:20:19 +0200

    add aws role creation for vmimport

--- ansible.mk
+++ ansible.mk
@@ -1,16 +1,7 @@
-ifeq ($(VERBOSE), true)
-ANSIBLE_ARGS                    += -v
-endif
-ifeq ($(DEBUG), true)
-ANSIBLE_ARGS                    += -vvvv
-endif
-ANSIBLE_ENV                     :=
-CMDS                            += ansible ansible-playbook
-
 .PHONY: ansible
-ansible:
-	$(call ansible, $(ANSIBLE_ARGS) $(ARGS))
+ansible: docker-build-ansible
+	$(call ansible,$(ANSIBLE_ARGS) $(ARGS))
 
 .PHONY: ansible-playbook
-ansible-playbook:
-	$(call ansible-playbook, $(ANSIBLE_ARGS) $(ARGS))
+ansible-playbook: docker-build-ansible
+	$(call ansible-playbook,$(ANSIBLE_ARGS) $(ARGS))
