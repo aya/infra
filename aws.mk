781e225cdc064094743eef2d659b0490d30915c4
Yann Autissier <yann.autissier@gmail.com>
2019-04-13 10:01:58 +0200

    wip

--- aws.mk
+++ aws.mk
@@ -2,8 +2,8 @@
 aws: docker-build-aws
 	$(call aws,$(ARGS))
 
-.PHONY: aws-role-create-import-snapshot
-aws-role-create-import-snapshot: aws-iam-create-role-$(AWS_SNAPSHOT_ROLE_NAME)  aws-iam-put-role-policy-$(AWS_SNAPSHOT_ROLE_NAME)
+.PHONY: aws-role-create-import-image
+aws-role-create-import-image: aws-iam-create-role-$(AWS_VM_IMPORT_ROLE_NAME)  aws-iam-put-role-policy-$(AWS_VM_IMPORT_ROLE_NAME)
 
 .PHONY: aws-iam-create-role-%
 aws-iam-create-role-%: docker-build-aws
@@ -26,6 +26,17 @@ aws-s3-cp: docker-build-aws
 .PHONY: aws-ec2-import-snapshot
 aws-ec2-import-snapshot: docker-build-aws
 	$(eval DRYRUN_IGNORE := true)
-	$(eval json := $(shell $(call exec,envsubst < aws/snapshot.json)))
+	$(eval json := $(shell $(call exec,envsubst < aws/import-snapshot.json)))
 	$(eval DRYRUN_IGNORE := false)
 	$(call aws,ec2 import-snapshot --disk-container '$(json)')
+
+.PHONY: aws-ec2-describe-import-snapshot-tasks
+aws-ec2-describe-import-snapshot-tasks: docker-build-aws
+	$(call aws,ec2 describe-import-snapshot-tasks)
+
+.PHONY: aws-ec2-register-image
+aws-ec2-register-image: docker-build-aws
+	$(eval DRYRUN_IGNORE := true)
+	$(eval json := $(shell $(call exec,envsubst < aws/register-image.json)))
+	$(eval DRYRUN_IGNORE := false)
+	$(call aws,ec2 register-image --image-location s3://$(AWS_SNAPSHOT_S3_BUCKET) '$(json)')
--- def.aws.mk
+++ def.aws.mk
@@ -1,7 +1,7 @@
 AWS_DEFAULT_REGION              ?= eu-west-1
 AWS_DEFAULT_OUTPUT              ?= text
 AWS_PROFILE                     ?= default
-AWS_SNAPSHOT_ROLE_NAME          ?= vmimport
+AWS_VM_IMPORT_ROLE_NAME         ?= vmimport
 AWS_SNAPSHOT_S3_BUCKET          ?= enova-aws-config
 AWS_SNAPSHOT_S3_KEY             ?= alpine-3.9.2-x86_64.iso
 CMDS                            += aws
