8c22472969a6e47752e78c3e8dfd6550648ba258
Yann Autissier <yann.autissier@gmail.com>
2019-05-16 13:50:35 +0200

    get default docker image from ec2 tag env

--- def.packer.alpine.mk
+++ def.packer.alpine.mk
@@ -1,5 +1,5 @@
 PACKER_ALPINE_ARCH              ?= x86_64
-PACKER_ALPINE_RELEASE           ?= 3.9.3
+PACKER_ALPINE_RELEASE           ?= 3.9.4
 PACKER_BUILD_VARS               += alpine_arch alpine_release alpine_version
 alpine_arch                     ?= $(PACKER_ALPINE_ARCH)
 alpine_release                  ?= $(PACKER_ALPINE_RELEASE)
