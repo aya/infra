8690898376c7c607ca869c3dcf3c08d07ef38d57
Yann Autissier <aya@users.noreply.github.com>
2019-07-09 14:27:26 +0200

    Merge pull request #1134 from crazyball/feature/api-newdocker
    feature: API php 7.3.x + dockers http / php

--- ansible.mk
+++ ansible.mk
@@ -24,5 +24,5 @@ ansible-run-%:
 .PHONY: ansible-ssh-run-%
 ansible-ssh-run-%: aws-ec2-get-PrivateIpAddress-1001pharmacies.$(ENV).$(APP)
 	$(eval ENV:=$*)
-	$(foreach host,$(AWS_INSTANCE_IP),$(call exec,ssh root@$(host) "make ansible-run ANSIBLE_TAGS=aws ANSIBLE_DOCKER_IMAGE_TAG=$(ANSIBLE_DOCKER_IMAGE_TAG)") &&) true
+	$(foreach host,$(AWS_INSTANCE_IP),$(call exec,ssh root@$(host) "make ansible-pull ANSIBLE_TAGS=aws") &&) true
 
