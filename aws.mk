92fa63c277bbe98f479e73649f222bb90e32fa1e
Yann Autissier <yann.autissier@gmail.com>
2019-12-12 18:47:02 +0100

    drone debug

--- aws.mk
+++ aws.mk
@@ -90,17 +90,18 @@ aws-ec2-describe-instance-PrivateIpAddress-%: docker-build-aws
 
 .PHONY: aws-ec2-get-PrivateIpAddress
 aws-ec2-get-PrivateIpAddress: docker-build-aws
-	$(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text)
-	$(shell $(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text))
+	$(eval DRYRUN_IGNORE := true)
 	$(eval AWS_INSTANCE_IP := $(shell $(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text) |sed $$'$$!N;s/\r\\n/ /' |awk '$$1 != "None" {print $$1}' 2>/dev/null))
-	echo coucou PrivateIpAddress: $(AWS_INSTANCE_IP)
+	$(eval DRYRUN_IGNORE := false)
+	echo PrivateIpAddress: $(AWS_INSTANCE_IP)
 
 .PHONY: aws-ec2-get-PrivateIpAddress-%
 aws-ec2-get-PrivateIpAddress-%: docker-build-aws
+	$(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text)
 	$(eval DRYRUN_IGNORE := true)
 	$(eval AWS_INSTANCE_IP := $(shell $(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text) |sed $$'$$!N;s/\r\\n/ /' |awk '$$1 != "None" && $$2 ~ /$*/ {print $$1}' 2>/dev/null))
 	$(eval DRYRUN_IGNORE := false)
-	echo PrivateIpAddress: $(AWS_INSTANCE_IP)
+	echo coucou PrivateIpAddress: $(AWS_INSTANCE_IP)
 
 .PHONY: aws-ec2-get-snap-id-import-snapshot-task
 aws-ec2-get-snap-id-import-snapshot-task: aws-ec2-get-snap-id-import-snapshot-task-$(AWS_TASK_ID)
