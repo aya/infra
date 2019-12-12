dfde0bde95dff19fdee79a8c39a08af1f9f507d4
Yann Autissier <yann.autissier@gmail.com>
2019-12-12 20:06:02 +0100

    drone debug

--- aws.mk
+++ aws.mk
@@ -102,10 +102,10 @@ aws-ec2-get-PrivateIpAddress-%: docker-build-aws
 	echo "-------------------------------------"
 	$(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text |sed $$'$$!N;s/\r\\n/ /')
 	echo "-------------------------------------"
-	$(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text |sed $$'$$!N;s/\r\\n/ /' |awk '$$1 != "None" && $$2 ~ /$*/ {print $$1}')
+	$(call aws,ec2 describe-instances --no-paginate --filter "Name=tag:Name,Values=$*" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 	echo "-------------------------------------"
 	$(eval DRYRUN_IGNORE := true)
-	$(eval AWS_INSTANCE_IP := $(shell $(call aws,ec2 describe-instances --no-paginate --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value$(comma)PrivateIpAddress]' --output text) |sed $$'$$!N;s/\r\\n/ /' |awk '$$1 != "None" && $$2 ~ /$*/ {print $$1}' 2>/dev/null))
+	$(eval AWS_INSTANCE_IP := $(shell $(call aws,ec2 describe-instances --no-paginate --filter "Name=tag:Name,Values=$*" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text) 2>/dev/null))
 	$(eval DRYRUN_IGNORE := false)
 	echo coucou PrivateIpAddress: $(AWS_INSTANCE_IP)
 
