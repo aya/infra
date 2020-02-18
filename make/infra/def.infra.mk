242196cd6196304523eae7e57a5f33eb74d12531
Yann Autissier <yann.autissier@gmail.com>
2020-02-18 09:43:54 +0100

    #r312 add newrelic to pre/prod stack

--- make/infra/def.infra.mk
+++ make/infra/def.infra.mk
@@ -18,7 +18,7 @@ SETUP_NFSD_OSX_CONFIG           ?= nfs.server.bonjour=0 nfs.server.mount.regular
 SETUP_SYSCTL                    ?= false
 SETUP_SYSCTL_CONFIG             ?= vm.max_map_count=262144 vm.overcommit_memory=1 fs.file-max=8388608 net.core.somaxconn=1024
 SHELL                           ?= /bin/sh
-STACK                           ?= logs $(if $(filter $(ENV),prod preprod),,services)
+STACK                           ?= logs $(if $(filter $(ENV),prod preprod),newrelic,services)
 STACK_BASE                      ?= base
 STACK_NODE                      ?= node
 
