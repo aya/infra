4ace8058705535fcf39987368d4660db910f370e
Ambrosini Loïc <loic@1001pharmacies.com>
2020-02-19 09:47:54 +0100

    Merge pull request #1392 from aya/fix/release/v3.97.0
    #r312 add newrelic configuration file in pre/prod docker images

--- make/app/def.app.mk
+++ make/app/def.app.mk
@@ -44,7 +44,7 @@ endef
 
 define install-parameters
 	$(eval path:=$(or $(1),$(APP)))
-	$(eval file:=$(or $(2),parameters.yml))
+	$(eval file:=$(or $(2),$(DOCKER_SERVICE)/parameters.yml))
 	$(eval dest:=$(or $(3),app/config))
 	$(eval env:=$(or $(4),$(ENV)))
 	$(if $(wildcard $(dest)/$(file)),,$(if $(wildcard ../parameters/$(env)/$(path)/$(file)),$(ECHO) cp -a ../parameters/$(env)/$(path)/$(file) $(dest)))
