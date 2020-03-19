d319b262d318af85e235f8c09154a66e1307f5ad
Yann Autissier <yann.autissier@gmail.com>
2020-03-19 11:54:28 +0100

    fix: drone build only projects impacted by merge

--- make/monorepo/def.monorepo.mk
+++ make/monorepo/def.monorepo.mk
@@ -11,8 +11,13 @@ SUBREPOS                        ?= $(filter subrepo/%, $(shell git remote))
 ifneq (,$(filter true,$(DRONE)))
 CONTEXT                         += DRONE_BRANCH DRONE_BUILD_EVENT DRONE_BUILD_NUMBER DRONE_COMMIT_AUTHOR DRONE_COMMIT_REF DRONE_COMMIT_SHA DRONE_TAG
 # APPS impacted by PR only
-ifneq (,$(filter $(DRONE_BUILD_EVENT),pull_request push))
-# prevent make tests down-rm to get through infra and tests folders
-APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(DRONE_COMMIT) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
+ifneq (,$(filter $(DRONE_BUILD_EVENT),pull_request))
+# filter-out to prevent make targets to get through DIRS folders
+APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(DRONE_COMMIT_SHA) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
+endif
+# APPS impacted by merge only
+ifneq (,$(filter $(DRONE_BUILD_EVENT),push))
+# filter-out to prevent make targets to get through DIRS folders
+APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(DRONE_COMMIT_SHA)^^ 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
 endif
 endif
