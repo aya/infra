d7121e112db3b76b63f31b02cec7cb7ee2a48fdb
Yann Autissier <yann.autissier@gmail.com>
2020-05-07 14:53:08 +0200

    fix drone tag deploy
    * drone deploys APPS impacted by the tag (since previous tag)

--- make/monorepo/def.monorepo.mk
+++ make/monorepo/def.monorepo.mk
@@ -9,23 +9,15 @@ SUBREPOS                        ?= $(filter subrepo/%, $(shell git remote))
 
 # CI/CD
 ifneq (,$(filter true,$(DRONE)))
-CONTEXT                         += DRONE_BRANCH DRONE_BUILD_EVENT DRONE_BUILD_NUMBER DRONE_COMMIT_AUTHOR DRONE_COMMIT_REF DRONE_COMMIT_SHA DRONE_TAG
-# limit APPS to those impacted by the PR
-ifneq (,$(filter $(DRONE_BUILD_EVENT),pull_request))
-# filter-out to prevent make targets to get through DIRS folders
-APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(DRONE_COMMIT_SHA) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
+ifneq (,$(filter $(DRONE_BUILD_EVENT),pull_request push))
+COMMIT_AFTER                    := $(DRONE_COMMIT_AFTER)
+COMMIT_BEFORE                   := $(DRONE_COMMIT_BEFORE)
 endif
-# limit APPS to those impacted by the merge
-ifneq (,$(filter $(DRONE_BUILD_EVENT),push))
-# filter-out to prevent make targets to get through DIRS folders
-APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(DRONE_COMMIT_SHA)^^ 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
-endif
-# limit APPS to those impacted by the tag
 ifneq (,$(filter $(DRONE_BUILD_EVENT),tag))
-# for hotfix only
-ifneq (0,$(lastword $(subst ., ,$(DRONE_TAG))))
-# filter-out to prevent make targets to get through DIRS folders
-APPS                            := $(filter-out $(DIRS), $(shell git diff --name-only origin/$(DRONE_BRANCH) $(shell git describe --abbrev=0 --tags $(DRONE_TAG)^) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}'))
+COMMIT_AFTER                    := $(DRONE_TAG)
+COMMIT_BEFORE                   := $(shell git describe --abbrev=0 --tags $(DRONE_TAG)^)
 endif
-endif
+# limit to APPS impacted by the commit
+APPS                            := $(sort $(filter-out $(DIRS), $(shell git diff --name-only $(COMMIT_BEFORE) $(COMMIT_AFTER) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}')))
+CONTEXT                         += DRONE_BRANCH DRONE_BUILD_EVENT DRONE_BUILD_NUMBER DRONE_COMMIT DRONE_COMMIT_AFTER DRONE_COMMIT_AUTHOR DRONE_COMMIT_BEFORE DRONE_COMMIT_REF DRONE_COMMIT_SHA
 endif
