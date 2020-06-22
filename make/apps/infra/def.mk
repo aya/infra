55f997adb331c7e9af2b1024fad2cf402ab3ba8b
Yann Autissier <yann.autissier@gmail.com>
2020-06-22 13:09:27 +0200

    chore: update parameters first
    * when installing for the first time, we should fire the
    install-parameters target first so infra-base is called with correct ssh
    hostname to build the docker base cli with functionnal ssh fingerprints

--- make/apps/infra/def.mk
+++ make/apps/infra/def.mk
@@ -9,6 +9,7 @@ ELASTICSEARCH_PROTOCOL          ?= http
 ENV_VARS                        += COMPOSE_IGNORE_ORPHANS DOCKER_IMAGE_CLI DOCKER_IMAGE_SSH DOCKER_NAME_CLI DOCKER_NAME_SSH ELASTICSEARCH_HOST ELASTICSEARCH_PASSWORD ELASTICSEARCH_PORT ELASTICSEARCH_PROTOCOL ELASTICSEARCH_USERNAME GIT_AUTHOR_EMAIL GIT_AUTHOR_NAME SETUP_SYSCTL_CONFIG SSH_BASTION_HOSTNAME SSH_BASTION_USERNAME SSH_PUBLIC_HOST_KEYS SSH_PRIVATE_IP_RANGE
 GIT_AUTHOR_EMAIL                ?= $(shell git config user.email 2>/dev/null)
 GIT_AUTHOR_NAME                 ?= $(shell git config user.name 2>/dev/null)
+GIT_REMOTE_HOSTS                ?= github.com gitlab.com
 HOME                            ?= /home/$(USER)
 MOUNT_NFS_CONFIG                ?= addr=$(MOUNT_NFS_HOST),actimeo=3,intr,noacl,noatime,nocto,nodiratime,nolock,soft,rsize=32768,wsize=32768,tcp,rw,vers=3
 MOUNT_NFS_DISK                  ?= $(MOUNT_NFS_HOST):/$(SHARED)
@@ -20,9 +21,9 @@ SETUP_NFSD_OSX_CONFIG           ?= nfs.server.bonjour=0 nfs.server.mount.regular
 SETUP_SYSCTL                    ?= false
 SETUP_SYSCTL_CONFIG             ?= vm.max_map_count=262144 vm.overcommit_memory=1 fs.file-max=8388608 net.core.somaxconn=1024
 SHELL                           ?= /bin/sh
-SSH_BASTION_HOSTNAME            ?= bastion.ssh
-SSH_BASTION_USERNAME            ?= user
-SSH_PUBLIC_HOST_KEYS            ?= github.com
+SSH_BASTION_HOSTNAME            ?= 
+SSH_BASTION_USERNAME            ?= 
+SSH_PUBLIC_HOST_KEYS            ?= $(GIT_REMOTE_HOSTS) $(SSH_BASTION_HOSTNAME)
 SSH_PRIVATE_IP_RANGE            ?= 10.10.*
 STACK                           ?= logs services
 
