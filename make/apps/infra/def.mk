18a59c85a91a186801dafc082c2b1b524c84731b
Yann Autissier <yann.autissier@gmail.com>
2020-06-23 15:42:42 +0200

    chore: update ssh_config if ssh-bastion is defined

--- make/apps/infra/def.mk
+++ make/apps/infra/def.mk
@@ -21,8 +21,8 @@ SETUP_NFSD_OSX_CONFIG           ?= nfs.server.bonjour=0 nfs.server.mount.regular
 SETUP_SYSCTL                    ?= false
 SETUP_SYSCTL_CONFIG             ?= vm.max_map_count=262144 vm.overcommit_memory=1 fs.file-max=8388608 net.core.somaxconn=1024
 SHELL                           ?= /bin/sh
-SSH_BASTION_HOSTNAME            ?= 
-SSH_BASTION_USERNAME            ?= 
+SSH_BASTION_HOSTNAME            ?=
+SSH_BASTION_USERNAME            ?=
 SSH_PUBLIC_HOST_KEYS            ?= $(GIT_REMOTE_HOSTS) $(SSH_BASTION_HOSTNAME)
 SSH_PRIVATE_IP_RANGE            ?= 10.10.*
 STACK                           ?= logs services
