307e75f049c3b9fff480b116b3459546df9e4cd4
Yann Autissier <yann.autissier@gmail.com>
2020-06-28 21:22:33 +0200

    chore: we want infra first !
    * infra should be the first APP in the list. When building/deploying
    APPS to pre/prod, they may depend on infra dockers. We need to push
    infra dockers before pulling them in other apps...

--- make/monorepo/def.drone.mk
+++ make/monorepo/def.drone.mk
@@ -8,7 +8,8 @@ ifneq (,$(filter $(DRONE_BUILD_EVENT),tag))
 COMMIT_AFTER                    := $(DRONE_TAG)
 COMMIT_BEFORE                   := $(shell git describe --abbrev=0 --tags $(DRONE_TAG)^)
 endif
+APPS_IMPACTED                   := $(shell git diff --name-only $(COMMIT_BEFORE) $(COMMIT_AFTER) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}')
 # prevent drone to make down in infra
-APPS                            := $(sort $(filter-out $(DIRS) $(if $(filter down,$(MAKECMDGOALS)),$(INFRA)), $(shell git diff --name-only $(COMMIT_BEFORE) $(COMMIT_AFTER) 2>/dev/null |awk -F '/' 'NF>1 && !seen[$$1]++ {print $$1}')))
+APPS                            := $(if $(filter-out down,$(MAKECMDGOALS)),$(filter $(INFRA),$(APPS_IMPACTED))) $(sort $(filter-out $(DIRS) $(INFRA),$(APPS_IMPACTED)))
 CONTEXT                         += DRONE_BRANCH DRONE_BUILD_EVENT DRONE_BUILD_NUMBER DRONE_COMMIT_AFTER DRONE_COMMIT_AUTHOR DRONE_COMMIT_BEFORE DRONE_COMMIT_REF
 endif
