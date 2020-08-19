abb0ff2e598113c2960889f7f9ef11d25d4d9c56
Yann Autissier <yann.autissier@gmail.com>
2020-08-19 14:28:24 +0200

    fix openstack latest alpine version

--- make/apps/infra/openstack.mk
+++ make/apps/infra/openstack.mk
@@ -1,7 +1,7 @@
 .PHONY: openstack
-openstack:
+openstack: docker-build-openstack
 	$(call openstack,$(ARGS))
 
 .PHONY: openstack-image-create
-openstack-image-create: $(PACKER_ISO_FILE)
+openstack-image-create: $(PACKER_ISO_FILE) docker-build-openstack
 	$(call openstack,$(OPENSTACK_ARGS) image create --disk-format raw --container-format bare --file $(PACKER_ISO_FILE) "$(PACKER_ISO_NAME)")
